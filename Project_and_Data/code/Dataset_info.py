1864dd720fad49dbbb55e98b4cf8ff5d36f8cf5a
ldemaddalena <161572190+ldemaddalena@users.noreply.github.com>
2024-04-22 17:26:06 +0200

    Update Dataset_info.py

--- Project_and_Data/code/Dataset_info.py
+++ Project_and_Data/code/Dataset_info.py
@@ -3,6 +3,9 @@ Hallo zusammen, Ich habe hier versucht eine File aufzusetzen, woran wir alle arb
 Man kann hier sehen was bereits gemacht wurde und vor allem auch wie es gemacht wurde um alles einheitlich zu gestalten.
 Das Dataset ist im selben Ordner wie diese File.
 
+- Ich habe einige print statements aukommentiert, alle auskommentierten code statements, welche man auch runnen könnte haben einen Abstand nach #
+  Alle Textkommentare sind direkt nach dem #
+- Alle Übertitel sind mit """ """ gekennzeichnet
 Environment:
 python == 3.11.8
 matplotlib=3.8.3
@@ -18,10 +21,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as sts
 
-
 data = pd.read_csv("../data/OWID-covid-data-28Feb2023.csv")
 
-#Getting an overview:
+
+
+"""GETTING AN OVERVIEW:"""
+
 # print(data.info())
 # print(data.describe())
 # print(data.head())
@@ -41,6 +46,27 @@ n_continents = data["continent"].nunique()
 #Locations per continent: (also to find all locations in nan)
 loc_in_con = data.groupby("continent")["location"].apply(list)
 # print(loc_in_con)  # -> now there is no more nan?
+
+
+
+"""DATA PREPROCESSING:"""
+
+#Total number of data entries
+print("Number of observations:", data.shape[0])
+#Datatypes of each variable:
+# print(data.dtypes)
+#Taking a closer look at data types:
+int_data = data.select_dtypes(include=("int64")).columns
+# print("INTEGERS: ", int_data)
+#We dont have any integers? -> We do but they are just not saved as such.
+float_data = data.select_dtypes(include=("float64")).columns
+# print("FLOATS: ", float_data)
+object_data = data.select_dtypes(include=("object")).columns
+# print("OBJECTS:", object_data)
+
+
+
+"""VISUALIZATIONS:""" 
 
 #Trying to find out what continent had how many cases total:
 cases_per_con = data.groupby("continent")["total_cases"].sum().reset_index()
