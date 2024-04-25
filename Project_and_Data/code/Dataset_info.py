4e37bfd1ffb9f47894b5dcd91b37a6bfb0fe83d1
ldemaddalena <161572190+ldemaddalena@users.noreply.github.com>
2024-04-25 10:06:53 +0200

    PreProcessing

--- Project_and_Data/code/Dataset_info.py
+++ Project_and_Data/code/Dataset_info.py
@@ -6,6 +6,7 @@ Das Dataset ist im selben Ordner wie diese File.
 - Ich habe einige print statements aukommentiert, alle auskommentierten code statements, welche man auch runnen könnte haben einen Abstand nach #
   Alle Textkommentare sind direkt nach dem #
 - Alle Übertitel sind mit """ """ gekennzeichnet
+
 Environment:
 python == 3.11.8
 matplotlib=3.8.3
@@ -31,6 +32,9 @@ data = pd.read_csv("../data/OWID-covid-data-28Feb2023.csv")
 # print(data.describe())
 # print(data.head())
 
+#Total number of data entries
+# print("Number of observations:", data.shape[0])
+
 #Number of locations and what locations:
 locations = data["location"].unique()  # -> not all are really countries (e.g. asia, africa etc.)
 # print(countries)
@@ -45,14 +49,17 @@ n_continents = data["continent"].nunique()
 
 #Locations per continent: (also to find all locations in nan)
 loc_in_con = data.groupby("continent")["location"].apply(list)
-# print(loc_in_con)  # -> now there is no more nan?
+# print(loc_in_con)  # -> now there is no more nan? there is no location saved under "the continent" nan?
 
-
+#Missing values:
+missing_values_per_variable = data.isnull().sum()
+# print(missing_values_per_variable)
+variables_with_no_missing_values = missing_values_per_variable[missing_values_per_variable == 0].index.tolist()
+# print(variables_with_no_missing_values)
+# print(missing_values_per_variable.sort_values(ascending=False))
 
 """DATA PREPROCESSING:"""
 
-#Total number of data entries
-print("Number of observations:", data.shape[0])
 #Datatypes of each variable:
 # print(data.dtypes)
 #Taking a closer look at data types:
@@ -62,22 +69,63 @@ int_data = data.select_dtypes(include=("int64")).columns
 float_data = data.select_dtypes(include=("float64")).columns
 # print("FLOATS: ", float_data)
 object_data = data.select_dtypes(include=("object")).columns
-# print("OBJECTS:", object_data)
+print("OBJECTS:", object_data)
+
+#Converting Date to a datetime variable
+data["date"] = pd.to_datetime(data["date"])
+# print(data.dtypes)
 
 
+#OWID EXPLANATION
+####################################################################################################################
+#We have two different styles of saving data, where I think OWID contains the continents and non-OWID contains countries -> I dont think they should be mixed?
+data_OWID = data[data['iso_code'].str.contains('OWID')] #Data frame containing OWID only
+# print(data_OWID)
+data_no_OWID = data.drop(data[data['iso_code'].str.contains('OWID')].index, axis=0) #Data frame containing all but OWID
+# print(data_no_OWID)
+grouped_OWID_iso = data_OWID.groupby("iso_code")
+iso_codes_OWID = list(grouped_OWID_iso.groups.keys()) #trying to find out what OWID is about
+# print(iso_codes_OWID)
+grouped_OWID_loc = data_OWID.groupby("location")
+loc_OWID = list(grouped_OWID_loc.groups.keys()) #OWID contains special summaries of data such as: Low income, European union as a whole etc.
+# print(loc_OWID)
+#I think all this OWID should be interpreted seperately from the rest of the data --> ASK TA
+#####################################################################################################################
+#USE DATA WITH RESPECT TO OWID AND NON OWID!!
+
+
+#Creating a dataframe containing all entries grouped by isocode -> to have data over whole time period:
+#getting a dataframe that contains sums and means according to column type
+#Will allow us to see what country had the most deaths, deaths per million, cases etc. over the given time period
+data_all_time_per_iso = data_no_OWID.groupby("iso_code").agg({col: ['min', 'max'] if col in ['date'] 
+                                                              else ('sum' if col in ['new_cases','new_deaths','new_cases_per_million','new_deaths_per_million','new_tests','new_tests_per_thousand','new_vaccinations'] 
+                                                                    else 'mean') for col in data_no_OWID.columns
+                                                                    if col not in ['iso_code', 'continent', 'location', 'tests_units']
+                                                                    and 'total' not in col.lower()
+                                                                    and 'smoothed' not in col.lower()
+                                                                    and 'excess' not in col.lower()})
+# print(data_all_time_per_iso.columns)
+# print(data_all_time_per_iso)
+#data_all_time_per_iso is a helpful dataframe containing averaged and total data per country, go and print the two lines above for more information
 
 """VISUALIZATIONS:""" 
 
-#Trying to find out what continent had how many cases total:
-cases_per_con = data.groupby("continent")["total_cases"].sum().reset_index()
-plt.figure(figsize=(12,8))
-fig1= sns.barplot(data=cases_per_con, x="continent", y="total_cases")
-plt.title("total cases per continent")
-plt.savefig("../output/tot_cases_per_cont.png")
+#Trying to find out what country had the most cases and cases per million:
 
-cases_per_con = data.groupby("continent")["total_cases_per_million"].sum().reset_index()
+tot_cases_per_country = data_all_time_per_iso["new_cases"]
+sorted_tot_cases_per_country = tot_cases_per_country.sort_values(ascending=False, by="sum").head(15)
 plt.figure(figsize=(12,8))
-fig2= sns.barplot(data=cases_per_con, x="continent", y="total_cases_per_million")
-plt.title("total cases per continent per million")
-plt.savefig("../output/tot_cases_per_cont_per_million.png")
+fig1= sns.barplot(data=sorted_tot_cases_per_country, x="iso_code", y="sum")
+plt.title("total cases per country")
+plt.savefig("../output/tot_cases_per_country.png")
+
+tot_cases_per_country_per_million = data_all_time_per_iso["new_cases_per_million"]
+sorted_tot_cases_per_country_per_million = tot_cases_per_country_per_million.sort_values(ascending=False, by="sum").head(15)
+plt.figure(figsize=(12,8))
+fig2= sns.barplot(data=sorted_tot_cases_per_country_per_million, x="iso_code", y="sum")
+plt.title("total cases per country per million")
+plt.savefig("../output/tot_cases_per_country_per_million.png")
+
+
+
 
